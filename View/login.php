30aa7095abe5f0194efce84f4f096b2b0ed695df
Alejandro Gonzalez <alexgve7@gmail.com>
2019-09-02 21:21:01 -0600

    updating const server url

--- View/login.php
+++ View/login.php
@@ -8,9 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <link rel="stylesheet" href="<?php print SERVERURL; ?>Imports/resources/css/global/materialize.min.css">
-    <link rel="stylesheet" href="<?php print SERVERURL; ?>Imports/resources/css/global/material-icons.css">
-    <link rel="stylesheet" href="<?php print SERVERURL; ?>Imports/resources/css/utilities/login.css">
+    <link rel="stylesheet" href="Imports/resources/css/global/materialize.min.css">
+    <link rel="stylesheet" href="Imports/resources/css/global/material-icons.css">
+    <link rel="stylesheet" href="Imports/resources/css/utilities/login.css">
 </head>
 <body>
     <div class="row">
@@ -43,9 +43,9 @@
         </div>
     </div>
 
-<script src="<?php print SERVERURL; ?>Imports/resources/js/global/jquery-3.2.1.min.js"></script>
-<script src="<?php print SERVERURL; ?>Imports/resources/js/global/materialize.min.js"></script>
-<script src="<?php print SERVERURL; ?>helpers/functions.js"></script>
-<script src="<?php print SERVERURL; ?>controllers/Login.js"></script>
+<script src="Imports/resources/js/global/jquery-3.2.1.min.js"></script>
+<script src="Imports/resources/js/global/materialize.min.js"></script>
+<script src="helpers/functions.js"></script>
+<script src="controllers/Login.js"></script>
 </body>
 </html>
